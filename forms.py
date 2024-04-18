98a79a27ec6831294dd44b97eff153aab9cc3312
devesh <deveshgupta3740@gmail.com>
2024-04-18 17:20:27 +0530

    updated files

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired, URL, Email, Length
 from flask_ckeditor import CKEditorField
+import psycopg2
+
 
 
 # WTForm for creating a blog post
